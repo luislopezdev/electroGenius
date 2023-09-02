fed917dc474fb0579c178209072bcd0efb7f2b56
Luis López Muñoz <luislp18000@gmail.com>
2023-09-01 20:02:09 -0400

    se agregaron mediaqueries y algunas funcionalidades de js

--- main.js
+++ main.js
@@ -69,16 +69,27 @@ productList.push({
     price:1000.000,
     Img:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
+productList.push({
+    name:'Notebook',
+    price:1000.000,
+    Img:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name:'Notebook',
+    price:1000.000,
+    Img:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+const product_details_container = document.querySelector('.product-details-container');
 for(product of productList){
-    //Manejo del doom
+    const product_container = document.createElement('div');
     const main_container = document.querySelector('.main-container');
     const product_img = document.createElement('img');
     const product_icon_add = document.createElement('img');
     const product_name = document.createElement('p');
     const product_price = document.createElement('p');
-    const product_container = document.createElement('div');
     const product_info = document.createElement('div');
     const container_price_name = document.createElement('div');
+    //Manejo del doom
     product_info.classList.add('product-info');
     product_container.classList.add('product-container');
     product_img.setAttribute('src',product.Img);
@@ -94,11 +105,36 @@ for(product of productList){
     container_price_name.append(product_price,product_name);
     product_info.append(container_price_name,product_icon_add);
     product_container.append(product_img,product_info);
-   
+    
+    product_container.addEventListener('click',()=>{
+        product_details_container.style.display = 'block';
+        console.warn('Prueba');
+    });
+   // product_info_container_btn_close = document.querySelector('.product-details-container');
+   // product_info_container_btn_close.addEventListener('click',()=>{
+        //console.log('sjk');
+    //});
 }
- //product info
- const product_details = document.querySelector('.product-details');
- product_details.append(product.price,product.name);
- product_img.addEventListener('click',()=>{
-     console.log('prueba');
- });
+//product info
+
+//product_details_container.style.display = 'block';
+//product_info_container_btn_close.classList.add('product-info-container-btn-close');
+const product_details = document.querySelector('.product-details');
+const product_info_img = document.createElement('img');
+product_info_img.setAttribute('src','https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+product_info_img.classList.add('product-info-img');
+const product_info_price = document.createElement('p');
+const product_info_name = document.createElement('p');
+ const product_info_description = document.createElement('p');
+ const product_info_btn_add_to_cart = document.createElement('button');
+ const product_info_container_btn_close = document.createElement('div');
+ const product_info_btn_close = document.createElement('img');
+ product_info_btn_close.setAttribute('src','./icons/icon_close.png');
+ product_info_container_btn_close.appendChild(product_info_btn_close);
+ product_details_container.appendChild(product_info_container_btn_close);
+ product_info_price.innerHTML = product.price;
+ product_info_name.innerHTML = product.name;
+ product_info_btn_add_to_cart.innerHTML = 'Add to cart'; 
+ product_info_description.innerHTML = 'With its practical position, this bike also fulfills a decorative function, add your hall or workspace.';
+ product_details_container.prepend(product_info_img);
+ product_details.append(product_info_price,product_info_name,product_info_description,product_info_btn_add_to_cart);
